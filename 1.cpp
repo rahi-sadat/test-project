c433752fd1684935d63fd9d7cb7ad3953f72f816
Rahi Sadat Ruhan <rahisadat7@gmail.com>
2025-10-13 03:09:08 +0600

    first commit: committing all the files

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int 
+
+void RUHAN(){
+ ll n;   cin>>n;
+vector<ll>v(n);
+ for(int i=0;i<n;i++)
+{
+cin>>v[i];
+}
+    
+}
+int main(){
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+    long long t;
+    cin>>t;
+    while(t--) RUHAN();
+}
